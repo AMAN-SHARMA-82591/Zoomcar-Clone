10e7b8c99219681115ba97bd82f21de3969f54dd
Aman Sharma <aman82591@gmail.com>
2021-08-17 07:44:21 +0530

    Added Files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RightArrow from '@material-ui/icons/ArrowForwardIos';
 import ArrowDown from '@material-ui/icons/KeyboardArrowDown';
 import PersonPointing from '../Images/bike10.jpeg';
@@ -6,22 +6,27 @@ import HeaderClickEvent from './HeaderClickEvent';
 
 
 let Header = () => {
+    let [click,setClick] = useState(false)
+   
 
     return(
-        <div className="header-section">
+        <div  className="header-section">
+        {click ?  <HeaderClickEvent /> : null}
             <img className="header-main-image" src={PersonPointing} alt="" />
             <div className="header-text-section">
                 <h1>DRIVE IN  A SANITIZED ZOOMCAR</h1>
-                <div className="header-cont-item-1">
+                <div   className="header-cont-item-1">
                     <p>Self Drive Car Rental in</p>
-                    <button type="submit">Select State <ArrowDown style={{fontSize:34}} /></button>
+                    <button onClick={() => setClick(!click)} type="submit">Select State <ArrowDown style={{fontSize:34}} /></button>
+                    
                 </div>
                 <div className="header-cont-item-2">
                     <p>Start your wonderful journey</p>
                     <RightArrow style={{color:"rgb(94, 86, 86)"}} />
                 </div>
+                
             </div>
-            <HeaderClickEvent />
+            
         </div>
     )
 }
--- src/components/HeaderClickEvent.js
+++ src/components/HeaderClickEvent.js
@@ -13,12 +13,20 @@ let images = [one,two,three, four,five,six,seven,eight,nine]
 let name = ['Chandigarh','Hyderabad','Banglore','Kolkata','Mumbai','Delhi NCR','Chennai','Vadodra','Pune']
 let name2 =['Nashik','Udupi-Manipal','Kochi','patna','Coimbatore','Indore','Guwahati','Calicut','Jaipur','Lucknow','Bhopal','Nagpur','Mysore','Raipur','Vijayawada','Bhubaneswar','Siliguri','Trichy','Vizag','Ahmedabad'];
 
-let HeaderClickEvent = () =>{
+
+
+
+let HeaderClickEvent = ({props}) =>{
     let [inputText,setValue] = useState("")
     let [result,setResult] = useState(false)
-    console.log(inputText)
+    let [check,setCheck] = useState(false)
+    let handleClickEvent = (event) =>[
+        setResult(event.target.id),
+        setCheck(event.type)
+    ]
+
     return(
-        <div className="header-click-event-section">
+        <div  className="header-click-event-section">
             <div className="header-content-sec-1">
                 <div className="header-click-main-heading">SELECT YOUR CITY</div>
                 <div className="header-click-event-input">
@@ -29,23 +37,25 @@ let HeaderClickEvent = () =>{
             <div className="header-content-sec-2">
                 <div>
                         <p style={{textAlign:'left',fontSize:"18px",letterSpacing:"1px"}}>Popular City</p>
-                        <ul className="header-click-list-item-1">
+                        <ul onClick={(e) => handleClickEvent(e)}  className="header-click-list-item-1">
                             {name.map((value,i) =>{
                                 return(
-                                    <li className={!inputText?'list-item-1':value.toLowerCase().includes(inputText.toLowerCase())?"list-item-1":"list-item-1 not-selected"} key={i}>
-                                        <img className="header-click-image" src={images[i]} alt="" />
+                                    <li id={i}  className={parseInt(result) === (i)?'list-item-1 selected':!inputText?'list-item-1':value.toLowerCase().includes(inputText.toLowerCase())?"list-item-1":"list-item-1 not-selected"} key={i}>
+                                        <img id={i} className="header-click-image" src={images[i]} alt={i} />
                                         {value}
                                     </li>
                                 )
                             })}
                     </ul>
                 </div>
+
+
                 <div style={{marginTop:"60px"}}>
                     <p style={{textAlign:"left",fontSize:"18px",letterSpacing:"1px"}}>Other Cities</p>
-                    <ul className="header-click-list-item-2">
+                    <ul onClick={(e) => handleClickEvent(e)} className="header-click-list-item-2">
                             {name2.map((value,i) =>{
                                 return (
-                                    <li className={!inputText?'list-item-2':value.toLowerCase().includes(inputText.toLowerCase())?"list-item-2":"list-item-2 not-selected"}  key={i}>
+                                    <li className={parseInt(result) === (i+9) && check ===  'click'?'list-item-2 selected':!inputText?'list-item-2':value.toLowerCase().includes(inputText.toLowerCase())?"list-item-2":"list-item-2 not-selected"} id={i+9} key={i+9}>
                                         {value}
                                     </li>
                                 )
